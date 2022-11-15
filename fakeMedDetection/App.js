587358521d38dd1990cfb813b7236f8ff3d11f91
Nishka Gosalia <nishka.gosalia@gmail.com>
2022-11-15 17:30:43 +0530

    basic changes in app using fetch

--- fakeMedDetection/App.js
+++ fakeMedDetection/App.js
@@ -13,9 +13,22 @@ import mongoose from 'mongoose';
 
 
 const App = () => {
-  dbConnect();
   const appId = 'fakemeddetection-rxjtd'; 
- 
+  fetch('http://127.0.0.1:3000/',{method:"GET"})
+      .then(function (response) {
+        // handle success
+        console.log(response)
+        alert(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        // handle error
+        alert(error.message);
+      })
+      .finally(function () {
+        // always executed
+        alert('Finally called');
+      });
+
   return (
     <SafeAreaView>
       <Text>Show the app</Text>
