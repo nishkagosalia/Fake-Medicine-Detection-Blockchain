4c3414cad4e999c390abeff66b61b3de7eacc083
tejanshu17 <tejanshu2001@gmail.com>
2022-11-29 08:31:00 +0530

    all changes for blockchain

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,24 @@
-// const http = require('http');
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type','text/plain');
-//     res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+import MetaCoin from '../fakeMedDetection/contractsOnly/MetaCoin.json';
 const express = require('express')
 const app = express()
 const hostname = '127.0.0.1';
 const port = 3000
-// textvariable = {
-//   'tejanshu':'nishka'
-// }
-
-// app.get('/', (req, res) => {
-//   res.send(textvariable)
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
   console.log(`Server running at http://${hostname}:${port}/`);
 })
 
+// blockchain
+// const Web3 = require('web3');
+// const web3 = new Web3("http://localhost:8545");
+// console.log(web3.eth.getAccounts());
+
+
+
+
+
+// database connection
 const { MongoClient } = require("mongodb");
  
 // Replace the following with your Atlas connection string                                                                                                                                        
@@ -79,11 +69,61 @@ app.get('/', (req, res) => {
 // the end point will be /login
 
 async function loginUser(username, password){
+  const web3 = await getWeb3();
+  const accounts = await web3.eth.getAccounts();
+  const networkID = await web3.eth.net.getId();
+  const deployedNetwork = MetaCoin.networks[networkID];
+  const instance = new web3.eth.Contract(
+    MetaCoin.abi,
+    deployedNetwork && deployedNetwork.address,
+  );
+
+  
+
+
+
+
+  // database retrieval
   const userDetails = await client.db("PharmaChain").collection("LoginReg").findOne({userName:username, password:password})
   if (userDetails.userName == username && userDetails.password == password){
     console.log("Login successful");
     app.get('/login', (req,res) => {
       res.send({userName:username, password:password})
+      // var Web3 = require('web3');
+      // var url ='HTTP://0.0.0.0:8545';
+      // var web3 = new Web3(url);
+      // web3.eth.getAccounts(console.log);
+
+      // var Accounts = require('web3-eth-accounts');
+      // var Web3 = new Accounts('HTTP://0.0.0.0:8545');
+      // Web3.eth.getAccounts(console.log);
+
+      
+      // const ganache = require("ganache");
+      // const options = {};
+      // const server = ganache.server(options);
+      // const PORT = 8545;
+      // server.listen(PORT, async err => {
+      //   if(err) throw err;
+      //   console.log('ganache is listening on port ${PORT}...');
+      //   const provider = server.provider;
+      //   const accounts = await provider.request(
+      //     {
+      //       method: "eth_accounts",
+      //       params:[]
+      //     }
+      //   );
+      //   console.log(accounts);
+      // });
+
+
+      
+
+
+
+      // const web3 = new Web3(ganache.provider());
+
+      
     }) 
   }
   else{
